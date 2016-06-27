e1b39efe2f966a0f25975676b17b21fc6bc8d084
Dave Tsang <dtsang@physics.mcgill.ca>
2016-06-27 13:39:31 +0100

    added mod_list option in order to prevent periodic variables from running away and compromising round-off precision.

--- slimplectic.py
+++ slimplectic.py
@@ -8,7 +8,7 @@ import slimplectic_GGL as ggl, numpy as np
 
 class GalerkinGaussLobatto(object):
 
-  def __init__(self, t, q_list, v_list):
+  def __init__(self, t, q_list, v_list, mod_list = False):
     # Validate inputs
     assert type(t) is str, "String input required."
     assert len(q_list) == len(v_list), "Unequal number of coordinates and velocities."
@@ -28,6 +28,15 @@ class GalerkinGaussLobatto(object):
     # Double the sympy variables
     self.qp, self.qm = ggl.q_Generate_pm(self.q)
     self.vp, self.vm = ggl.q_Generate_pm(self.v)
+    
+    #keep track of which variables are periodic and need to be modded
+    if mod_list:
+        self.modlist = mod_list
+    if not mod_list:
+        self.modlist = []
+        for q in q_list:
+            self.modlist.append(False)
+    
 
   def keys(self):
     return self.__dict__.keys()
@@ -44,7 +53,7 @@ class GalerkinGaussLobatto(object):
 
     # Check if total Lagrangian is discretized already
     if not hasattr(self, '_qi_soln_map'):
-      raise AttributeError("Run `discretize` to discretize the total Lagrangian.")
+        raise AttributeError("Run `discretize` to discretize the total Lagrangian.")
 
     # Validate input
     assert type(q0_list) in [list, np.ndarray], "List or numpy array input required."
@@ -57,15 +66,28 @@ class GalerkinGaussLobatto(object):
 
     # Set initial data
     q_list_soln[0,:] = q0_list
+    # mod the value of any periodic variables that have mod value specified
+    # This prevents NC evolution error due to increasing roundoff error as 
+    # any cyclic periodic variables become large. 
+    for jj,mod in enumerate(self.modlist):
+        if mod:
+            q_list_soln[0,jj] = q_list_soln[0,jj]%mod
     pi_list_soln[0,:] = pi0_list
+
 
     # Perform the integration at fixed time steps
     dt = t[1]-t[0]
     for ii in range(1, t_len):
-      args = [q_list_soln[ii-1], pi_list_soln[ii-1], t[ii-1], dt]
-      qi_sol = self._qi_soln_map(*args)
-      q_list_soln[ii] = self._q_np1_map(qi_sol, *args)
-      pi_list_soln[ii] = self._pi_np1_map(qi_sol, *args)
+        args = [q_list_soln[ii-1], pi_list_soln[ii-1], t[ii-1], dt]
+        qi_sol = self._qi_soln_map(*args)
+        q_list_soln[ii] = self._q_np1_map(qi_sol, *args)
+        # mod the value of any periodic variables that have mod value specified
+        # This prevents NC evolution error due to increasing roundoff error as 
+        # any cyclic periodic variables become large. 
+        for jj,mod in enumerate(self.modlist):
+            if mod:
+                q_list_soln[ii,jj] = q_list_soln[ii,jj]%mod
+        pi_list_soln[ii] = self._pi_np1_map(qi_sol, *args)
 
     # Return the numerical solutions
     return q_list_soln.T, pi_list_soln.T
