ae925f279c99a29709817f0aeb0ab7d041228df9
David Tsang <dtsang@astro.umd.edu>
2016-08-31 14:40:48 -0400

    fixed docstring

--- slimplectic.py
+++ slimplectic.py
@@ -78,7 +78,7 @@ class GalerkinGaussLobatto(object):
           output_v: Boolean value, if True, output will be 
                     q_list_soln.T, pi_list_soln.T, qdot_list_soln.T
                     defaults to False
-          outputFile: string for the output file. Defaults to False and no file is produced.
+          outputFile: filename for the output file. Defaults to False and no file is produced.
                       output is in csv format, with first line the column labels. 
           t_out: sets the output t_array for the output_File. Defaults to same as t
           print_steps: outputs to file every print_steps steps. Defaults to 1. 
